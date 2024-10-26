df1b114fe5925c9fede754b4ac9e1007276b39d3
mrochelle23 <mrochellebiz@gmail.com>
2024-10-25 22:15:18 -0400

    Updated the file

--- script.sh
+++ script.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-list_of_sizes=(8, 16, 32)
+# Define an array
+list_of_sizes=(8 16 32)
 
+# Loop through each program name passed as an argument
 for program in "$@"; do
-    
+    # Create the output file name by appending .out to the program name
     output_file="${program}.out"
-    
-    > "$output_file"
-    
+
+    # Create the output file
+    : > "$output_file"
+
+    # Loop through each size in the list_of_sizes array
     for size in "${list_of_sizes[@]}"; do
-    
+        # Run the program with the current size and save the output
         python3 "${program}.py" "$size" >> "$output_file"
-    
     done
 done
